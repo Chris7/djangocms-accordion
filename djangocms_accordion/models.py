900187650e8dab5457c2fdf69fd26d28b71e08f4
Chris Mitchell <chris.mit7@gmail.com>
2014-10-02 16:44:44 -0400

    added support for bootstrap accordions

--- djangocms_accordion/models.py
+++ djangocms_accordion/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class Accordion(CMSPlugin):
     custom_classes = models.CharField(_('custom classes'), max_length=200, blank=True)
+    bootstrap = models.BooleanField(_('Bootstrap Styled Accordion'), default=False)
 
     def __unicode__(self):
         return _(u"%s columns") % self.cmsplugin_set.all().count()
